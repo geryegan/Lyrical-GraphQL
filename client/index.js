18da9e89156069cc99d27739c820ccd74969dfd2
Gery Egan <gery.egan@gmail.com>
2017-06-19 13:58:28 -0700

    apollo provider and client

--- client/index.js
+++ client/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
-const Root = () => {
-  return <div>Lyrical</div>
-};
+const client = new ApolloClient({});
+
+const Root = () => (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 
 ReactDOM.render(
   <Root />,
