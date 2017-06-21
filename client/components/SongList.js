8778ccde0743f4f5821610872b8d1f6b9421dcb3
Gery Egan <gery.egan@gmail.com>
2017-06-20 17:32:13 -0700

    gql-tag, graphql import, song title query written

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+    componentDidMount() {
+
+    }
     render() {
         return (
            <div>Song List</div> 
         );
     }
 }
+
+const query = gql`
+{
+    songs {
+        title
+    }
+}
+`;
 
 export default SongList;
