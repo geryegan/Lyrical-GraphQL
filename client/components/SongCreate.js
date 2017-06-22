3b245d2b52de1a6e7d3fbac64b17e40df7bd1041
Gery Egan <gery.egan@gmail.com>
2017-06-21 18:21:00 -0700

    SongCreate component

---
+++ client/components/SongCreate.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+
+    render() {
+        return (
+          <div>
+              <h3>
+                  Song Create
+              </h3>
+              <form></form>
+          </div>  
+        );
+    }
+}
+
+export default SongCreate;
