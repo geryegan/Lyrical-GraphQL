232db4e38baf9333ae74e509e0ce2b6b5aed538a
Gery Egan <gery.egan@gmail.com>
2017-06-26 23:55:20 -0700

    code cleanup

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -23,7 +23,6 @@ class SongList extends Component {
     }
 
     render() {
-        console.log(this.props);
         if (this.props.data.loading) { return <div>Loading</div>; }
         return (
             <div>
