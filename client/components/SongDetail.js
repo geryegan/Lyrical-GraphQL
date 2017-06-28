029aba473f68e9a5d01b581e923bed7140c42203
Gery Egan <gery.egan@gmail.com>
2017-06-27 18:37:20 -0700

    graphql wiring

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import getSong from '../queries/getSong';
 
 class SongDetail extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>
-                <h3>Song Detail</h3>
+                <h3>Song Detail, {this.props.match.params.id} </h3>
             </div>
         );
     }
 }
 
-export default SongDetail;
+export default graphql(getSong, {
+    options: (props) => ({ variables: { id: props.match.params.id } })
+})(SongDetail);
