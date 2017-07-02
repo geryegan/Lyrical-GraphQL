1a0d0d028d08fd6822227684083fff1c870516e9
Gery Egan <gery.egan@gmail.com>
2017-07-02 14:46:09 -0700

    refetch logic, lyricList

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,20 +3,26 @@ import { graphql } from 'react-apollo';
 import { Link } from 'react-router-dom';
 import getSong from '../queries/getSong';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
+    // componentDidUpdate() {
+    //     this.props.data.refetch();
+    // }
     render() {
+        console.log('yeah', this.props);
         const { song } = this.props.data;
         if (!song) {
             return <div>Loading</div>;
         }
-        console.log(this.props);
+        
         return (
                 <div>
                     <Link to="/">
                       Back
                     </Link>
                     <h3>{song.title}</h3>
+                    <LyricList lyrics={song.lyrics} />
                     <LyricCreate songId={song.id} />
                 </div>
         );
